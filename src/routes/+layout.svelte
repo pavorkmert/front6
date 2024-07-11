daf3b90939aba9e8ce814d2625e9c7ef262f64db
Mark <76394742+pavorkmert@users.noreply.github.com>
2024-07-11 20:46:33 +0300

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<Footer />
+
+<style>
+/* Ваши стили */
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: dev ? '' : '/my-portfolio'
+    },
+    prerender: {
+      default: true
+    }
+  }
+};
